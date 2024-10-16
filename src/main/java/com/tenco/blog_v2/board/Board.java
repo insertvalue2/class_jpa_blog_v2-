d182c2838fda7a07a291aa2bbf6911a13e3c6333
kdt4 <insertvaue@naver.com>
2024-10-16 10:07:58 +0900

    reply 테이블 설계

--- src/main/java/com/tenco/blog_v2/board/Board.java
+++ src/main/java/com/tenco/blog_v2/board/Board.java
@@ -1,10 +1,13 @@
 package com.tenco.blog_v2.board;
 
+import com.tenco.blog_v2.reply.Reply;
 import com.tenco.blog_v2.user.User;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @Entity
@@ -33,6 +36,11 @@ public class Board {
     // 즉, JPA 메모리상에서만 활용 가능한 필드 이다.
     @Transient
     boolean isBoardOwner;
+
+    // 댓글 엔티티를 넣어서 관계 설정하면 -- 양방향
+    @OneToMany(mappedBy = "board", fetch = FetchType.LAZY)
+    private List<Reply> replies = new ArrayList<>(); // 빠른 초기화
+
 
     @Builder
     public Board(Integer id, String title, String content, User user, Timestamp createdAt) {
